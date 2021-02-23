71c1d4480565de208011019d15a65d6aac6a46c4
Shogo <vanitas710@yahoo.co.jp>
2021-02-24 01:41:36 +0900

    画像の表示 丸と四角のクリッピング 影をつける X軸に回転をつけた

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("Hello Apple")
+                    .font(.system(size: 50))
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .rotation3DEffect(
+                        .degrees(45),
+                        axis: (x: 1.0, y: 0.0, z: 0.0)
+                    )
+                    .padding()
+                Image("スクリーンショット 2021-02-24 1.24.02")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 300, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .padding(.all)
+                    .shadow(radius: 50)
+                
+                Image("スクリーンショット 2021-02-24 1.29.05")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 300, height: 300)
+                    .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+                    .padding(.all)
+                    .shadow(radius: 50)
+            }
+            
+        }
     }
 }
 

6ef53969073c5d0859c27f2cfe73a1e42bcfdcb6
Shogo <vanitas710@yahoo.co.jp>
2021-02-23 23:50:26 +0900

    Itaric調にするのとOffsetで位置をずらす

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -17,9 +17,12 @@ struct ContentView: View {
                 Text("iPhone")
                 Text("iPad")
                 Text("Apple Watch")
+                    .italic()
+                    .offset(x: -50, y: 0)
             }
             .padding(.all)
-            Spacer()
+            .padding(.top,80)
+            
             VStack(alignment: .trailing, spacing: 15.0) {
                 Text("Macintosh")
                 Text("iPhone")
@@ -27,7 +30,16 @@ struct ContentView: View {
                 Text("Apple Watch")
             }
             .padding(.all)
-            Spacer()
+            .padding(.top,100)
+            
+            
+            VStack {
+                Text("Apple")
+                    .font(.system(size: 70))
+                Text("Designed by Callfornia")
+                    .italic()
+                    .offset(x: 50, y: 0)
+            }
         }
         
         

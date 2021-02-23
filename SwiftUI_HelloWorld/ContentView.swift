73ceb71fe9f83cd5da80c2a4c7ef9d76a666437e
Shogo <vanitas710@yahoo.co.jp>
2021-02-23 22:24:10 +0900

    Initial Commit

---
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_HelloWorld
+//
+//  Created by Shogo Nobuhara on 2021/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_HelloWorld/SwiftUI_HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HelloWorldApp.swift
+//  SwiftUI_HelloWorld
+//
+//  Created by Shogo Nobuhara on 2021/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

df86f4ff670d9067c96fe2ffdd3c6d1599a0b31a
Shogo <vanitas710@yahoo.co.jp>
2021-02-24 02:53:47 +0900

    リスト表示を行ってみた。 Viewを分けてコードを書いてみた。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+        List {
+            Text("Content")
+            Text("Content")
+            Photo1().frame(height: 150)
+            Text("Content")
+            Text("Content")
+            Photo2().frame(height: 150)
+        }
+    }
+}
+
+
+// 写真1
+struct Photo1: View {
+    var body: some View {
+        HStack {
+            Image("Apple_watch-series-6-aluminum-blue-case_09152020")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("Blue Alluminium")
+                .padding(.horizontal)
+        }
+    }
+}
+
+// 写真2
+struct Photo2: View {
+    var body: some View {
+        HStack {
+            Image("Apple_watch-series-6-aluminum-red-case_09152020")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("Red Alluminium")
+                .padding(.horizontal)
+        }
     }
 }
 

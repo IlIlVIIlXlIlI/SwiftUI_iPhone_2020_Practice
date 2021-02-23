e880418c9cb243e5702fb910d2aae85831d6bda9
Shogo <vanitas710@yahoo.co.jp>
2021-02-23 23:25:20 +0900

    フォントの大きさ、空白、色、右よせなどを追加

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("iPhone12 Pro")
+                .font(.title)
+                .fontWeight(.thin)
+                .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+                .border(Color.blue,width: 5)
+            
+            Text("Say Hello to 5G")
+                .font(.system(size: 100))
+                .foregroundColor(Color.blue)
+                .padding()
+            Text("5GをProで。どんなスマートフォンのチップよりも飛び抜けて高性能なA14 Bionic。")
+                .multilineTextAlignment(.leading)
+                .frame(width: 200.0, height: 100.0)
+                
+        }
     }
 }
 

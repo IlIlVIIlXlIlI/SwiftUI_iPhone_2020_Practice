f228afa8839257e4345ec751dd16c850fa2490cd
Shogo <vanitas710@yahoo.co.jp>
2021-02-25 03:07:48 +0900

    簡易なナビゲーションリンクを作成した。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink(
+                destination: SubView()) {
+                Text("Go to SubView")
+            }
+        }
+    }
+}
+
+struct SubView: View {
+    var body: some View {
+        Text("SubView")
     }
 }
 

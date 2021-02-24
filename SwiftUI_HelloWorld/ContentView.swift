25ce0759f38327e40fa7f9b076f191e475acd56e
Shogo <vanitas710@yahoo.co.jp>
2021-02-25 01:48:19 +0900

    ForEach文を初めて使った。 リストを複数にした。 GroupedListStyle()でいい感じになった。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -10,20 +10,27 @@ import SwiftUI
 let iPhone = ["iPhone","iPhone3G","iPhone3GS","iPhone4","iPhone4S","iPhone5",
 "iPhone6","iPhone6S","iPhone7","iPhone8","iPhoneX","iPhoneXS","iPhone11","iPhone12"]
 
+let appleWatch = ["Apple Watch","Apple Watch Series1","Apple Watch Series2","Apple Watch Series3","Apple Watch Series4","Apple Watch Series5","Apple Watch Series6","Apple Watch Series SE"]
+
 struct ContentView: View {
     var body: some View {
         
-        NavigationView() {
-            List(0 ..< iPhone.count) { item in
-                HStack {
-                    Text(String(item))
-                    Text(iPhone[item])
-                        .padding()
+        List {
+            Section(header: Text("iPhone").font(.largeTitle).padding(.top),
+                    footer: Text("Designed by Callfornia")) {
+                ForEach(0..<iPhone.count) { index in
+                    Text(iPhone[index])
                 }
             }
-            .padding(.top)
-            .navigationTitle("All iPhone")
+            
+            Section(header: Text(" Watch").font(.largeTitle).padding(.top),
+                    footer: Text("Designed by Callfornia")) {
+                ForEach(0..<appleWatch.count) { index in
+                    Text(appleWatch[index])
+                }
+            }
         }
+        .listStyle(GroupedListStyle())
     }
 }
 

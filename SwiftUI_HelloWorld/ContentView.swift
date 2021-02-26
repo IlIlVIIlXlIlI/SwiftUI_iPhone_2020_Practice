74deaac14535a0af66f4ffea5e8455ff5d56167c
Shogo <vanitas710@yahoo.co.jp>
2021-02-27 05:21:55 +0900

    写真のフェードインアウトをボタンで行えるように

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShow = true
+    @State var opaValue = 1.0
+    @State var btnText = "消す"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                isShow.toggle()
+                btnText = isShow ? "消す" : "表示する"
+                withAnimation(.easeInOut(duration: 2)){
+                    opaValue = isShow ? 1.0 : 0.0
+                }
+            }) {
+                Text(btnText).font(.title2)
+            }
+            // 写真
+            Image("apple2020")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.5)
+        }
     }
 }
 

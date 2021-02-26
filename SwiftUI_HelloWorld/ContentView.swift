ec13faf31c0d9bcbe27278696e58961af7b45c83
Shogo <vanitas710@yahoo.co.jp>
2021-02-27 03:25:20 +0900

    URLのリストを作って、表示した

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+struct webData: Identifiable {
+    var id = UUID()
+    var name:String
+    var url:String
+    var favicon:String
+}
+
 struct ContentView: View {
+    
+    // Webデータリストを作る
+    let webList = [
+        webData(name: "アップル", url: "https://apple.com/jp/", favicon: "apple"),
+        webData(name: "東京国立博物館", url: "https://www.tnm.jp", favicon: "tnm"),
+        webData(name: "東京都現代美術館", url: "https://www.mot-art-museum.jp", favicon: "mo"),
+        webData(name: "川崎水族館", url: "https://kawa-sui.com", favicon: "kawa-sui")
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            List(webList) { item in
+                // ファビコン
+                Image(item.favicon)
+                    .resizable()
+                    .frame(width: 40,height: 40)
+                
+                // リンクを作る
+                Link(item.name, destination: URL(string: item.url)!)
+            }
+        }
+        .navigationBarTitle("Webリスト")
     }
 }
 

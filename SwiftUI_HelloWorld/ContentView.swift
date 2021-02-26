7a4fa48bc5a7e6e99934365f33f028db4248f248
Shogo <vanitas710@yahoo.co.jp>
2021-02-27 04:08:36 +0900

    ボタンを作った。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var num:Int = 0
+    @State var msg:String = "Thank you!"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                num = Int.random(in: 0...100) // 0-100のランダム
+                //print(num)
+            }) {
+                Text("Random Button")
+                    .font(.largeTitle)
+                    .frame(width: 280, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(15,antialiased: true)
+            }
+            // 結果を表示するテキスト
+            Text("\(num)")
+                .font(.largeTitle)
+                .padding()
+        }
+        
+        VStack {
+            Button("Tap") {
+                if msg == "Thank you!" {
+                    msg = "ありがとう！"
+                } else {
+                    msg = "Thank you!"
+                }
+            
+            }
+            .font(.headline)
+            .foregroundColor(.white)
+            .background(
+                Capsule()
+                    .foregroundColor(.green)
+                    .frame(width: 80, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            )
+            // 結果の表示
+            Text(msg).padding()
+        }
     }
 }
 

2b93362c0f350dddeb81e28c41531d2331be387d
Shogo <vanitas710@yahoo.co.jp>
2021-02-27 05:01:07 +0900

    トグルボタンでスイッチのオン/オフを行えるように。 ボタンを押した際にアニメーションするようにした。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var iLike: Bool  = true
+    @State var isFast: Bool = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Toggle(isOn: $iLike) {
+                Text("Like or Not")
+                    .font(.largeTitle)
+            }
+            .fixedSize()
+            .padding(60)
+            
+            // 選んだ結果で分岐する
+            if iLike {
+                Image(systemName: "heart.fill")
+                    .font(.system(size: 80))
+                    .foregroundColor(.red)
+            } else {
+                Image(systemName: "heart.slash")
+                    .font(.system(size: 80))
+                    .foregroundColor(.gray)
+            }
+        }
+        
+        
+        HStack {
+            Button(action: {
+                withAnimation {
+                    isFast.toggle()
+                }
+            }){
+                // ボタン名の変更
+                Text(isFast ? "RUN" : "STOP")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(isFast ? .red : .black)
+                    .padding(.trailing)
+                    .frame(width:100)
+                // イメージの変更
+                Image(systemName: isFast ? "hare" : "tortoise")
+                    .foregroundColor(isFast ? .blue : .green)
+                    .rotationEffect(.degrees(isFast ? 0 : 180))
+                    .scaleEffect(3)
+                    .frame(width:100)
+            }
+        }
     }
 }
 

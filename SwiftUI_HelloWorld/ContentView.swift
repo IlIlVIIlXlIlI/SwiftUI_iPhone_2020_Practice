d2e8f87130275bb899fa6c7cbd7b60e3ad529315
Shogo <vanitas710@yahoo.co.jp>
2021-02-28 01:30:40 +0900

    ステッパーを作った。 +-5ずつ変わるステッパーを作った。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var kosu: Int = 0
+    let tanka = 240
+    let tax = 0.1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(alignment: .leading, spacing: 30) {
+            Text("5個ずつ30個まで、1個\(tanka)円")
+                .font(.headline)
+            
+            Stepper(
+                onIncrement: {
+                    kosu += 5
+                    kosu = min(kosu,30)
+                },
+                onDecrement: {
+                    kosu -= 5
+                    kosu = max(kosu,0)
+                },
+                label: {Text("個数: \(kosu)")}
+            )
+            .frame(width: 200)
+            
+            Text("料金:\(calc(kosu))円 [税込]").underline()
+        }
+    }
+    
+    // 料金の計算
+    func calc(_ num:Int) -> Int {
+        let price = tanka * num
+        let result = Double(price) * (1+tax)
+        return Int(result)
     }
 }
 

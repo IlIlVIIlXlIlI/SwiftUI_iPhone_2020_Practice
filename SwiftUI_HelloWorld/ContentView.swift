d75f1ef07066f4be3bca531e90cd8777ef256cea
Shogo <vanitas710@yahoo.co.jp>
2021-02-28 03:44:44 +0900

    セグメンテッドコントロールを作った。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedColor = 0
+    let colorViews = [Color.red,Color.green,Color.blue]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker(selection: $selectedColor, label: Text("Color"), content: {
+                Text("Red").tag(0)
+                Text("Green").tag(1)
+                Text("Blue").tag(2)
+            })
+            .pickerStyle(SegmentedPickerStyle())
+            
+            symbolImage(num: selectedColor)
+                .resizable()
+                .foregroundColor(colorViews[selectedColor])
+                .frame(width: 100, height: 100)
+                .padding()
+        }
+        .padding()
     }
+    
+    func symbolImage(num:Int) -> Image {
+        switch num {
+        case 0:
+            return Image(systemName: "r.circle")
+        case 1:
+            return Image(systemName: "g.circle")
+        case 2:
+            return Image(systemName: "b.circle")
+        default:
+            return Image(systemName: "r.circle")
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

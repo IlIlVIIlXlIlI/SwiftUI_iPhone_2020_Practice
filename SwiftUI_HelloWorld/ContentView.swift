5d68764da0e0de52d4f1b96627fd1b9af2d59685
Shogo <vanitas710@yahoo.co.jp>
2021-02-28 03:18:36 +0900

    2つのピッカーができたところ

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedSize = 2
+    @State var selectedColor = 0
+    let sizes = ["XS","S","M","L","LL"]
+    let colors = ["Red","Green","Blue","Yellow","Pink","White"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                // sizeのピッカー
+                Picker(selection: $selectedSize, label: Text("Size"), content: {
+                    ForEach(0..<sizes.count) { index in
+                        Text(sizes[index])
+                    }
+                })
+                .frame(width: 150)
+                .padding(.horizontal,10)
+                .clipped()
+                
+                // Colorのピッカー
+                Picker(selection: $selectedColor, label: Text("Color"), content: {
+                    ForEach(0..<colors.count) { index in
+                        Text(colors[index])
+                    }
+                })
+                .frame(width: 150)
+                .padding()
+                .clipped()
+                .onAppear(perform: {
+                    selectedColor = colors.count/2
+                })
+                
+            }
+        }
     }
 }
 

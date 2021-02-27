cc0d821f6be4b62d9fe3586a9fab0bf5ea5e995c
Shogo <vanitas710@yahoo.co.jp>
2021-02-28 03:07:51 +0900

    選んだホールの配列から色が変わるようにした。

--- SwiftUI_HelloWorld/ContentView.swift
+++ SwiftUI_HelloWorld/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedColor = 0
+    let colorViews = [Color.red,Color.green,Color.blue]
+    let colorNames = ["Red","Green","Blue"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Picker(selection: $selectedColor, label: Text("Color")) {
+                Text("Red").tag(0)
+                Text("Green").tag(1)
+                Text("Blue").tag(2)
+            }
+            
+            HStack {
+                colorViews[selectedColor]
+                    .frame(width: 50,height: 100)
+                Text("Value: \(selectedColor)")
+                    .frame(width: 70)
+                Text("\(colorNames[selectedColor])")
+                    .frame(width: 70)
+            }
+            
+        }
+        .padding()
     }
 }
 
